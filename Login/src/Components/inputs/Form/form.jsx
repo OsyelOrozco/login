ff1cb93da42a8f326d736b299c905bcf4f7855d0
OsyelOrozco <oorozcoc3@miumg.edu.gt>
2025-04-10 17:16:13 -0600

    Cracion de from
    From

---
+++ Login/src/Components/inputs/Form/form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const form = () => {
+  return (
+    <section className='register'>
+        <form >
+            <h3>Resgidtro
+            </h3>
+        </form>
+    </section>
+        
+  )
+}
+
+export default form
